19eda05da0d5a361249b4cffbefd11a70f858e07
Benjamin Ting <tingzhb@gmail.com>
2026-09-02 11:34:57 +0200

    Create GET Request to Cat Facts API.
    Error handling included!

--- SampleClient/Program.cs
+++ SampleClient/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello World!");
+﻿string catFactUrl = "https://catfact.ninja/fact";
+
+HttpClient client = new HttpClient();
+
+// GET Request
+try{
+	string response = await client.GetStringAsync(catFactUrl);
+	Console.WriteLine(response);
+}
+catch (HttpRequestException e){
+	Console.WriteLine(e.Message);
+}
+
+
+
